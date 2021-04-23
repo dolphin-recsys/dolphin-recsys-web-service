ee484ad44d85725a8c208aada23e4c37488380a1
shxtwang <shxtwang@cn.ibm.com>
2021-04-23 20:06:06 +0800

    init the web service

---
+++ src/main/java/com/recsys/dolphin/ApplicationBootStrap.java
@@ -0,0 +1,16 @@
+package com.recsys.dolphin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kris
+ */
+@SpringBootApplication
+public class ApplicationBootStrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBootStrap.class, args);
+    }
+
+}
